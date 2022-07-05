3d2a40cedac63d5583226ecb117a442dae01f23a
mark-escosura <escosuramarkse@gmail.com>
2022-07-04 18:36:53 -0700

    WIP: Created getRecipes and getRecipesById model function

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,7 +1,18 @@
-function getRecipeById(recipe_id) {
-  return Promise.resolve(`awesome recipe number ${recipe_id}`);
+const db = require('../../data/db-config');
+
+async function getRecipes() {
+  const recipeRows = await db('recipes as r');
+
+  return recipeRows;
+}
+
+async function getRecipeById(recipe_id) {
+  const recipe = await db('recipes').where('recipe_id', recipe_id);
+
+  return recipe;
 }
 
 module.exports = {
+  getRecipes,
   getRecipeById,
 };
--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,6 +1,14 @@
 const router = require('express').Router();
 const Recipe = require('./recipes-model');
 
+router.get('/', (req, res, next) => {
+  Recipe.getRecipes()
+    .then((resource) => {
+      res.json(resource);
+    })
+    .catch(next);
+});
+
 router.get('/:recipe_id', (req, res, next) => {
   Recipe.getRecipeById(req.params.recipe_id)
     .then((resource) => {
